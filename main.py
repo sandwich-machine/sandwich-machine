2c52827f4a7d98579a77792e2c24aa264e8841ad
Soup <77903666+SoupDevHub@users.noreply.github.com>
2021-04-09 15:17:40 -0700

    Made main.py actually work

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ print('Random Sandwiches')
 
 types = ['classic', 'grilled', 'double', 'bagel']
 
-if types == 'classic':
+if choice(types) == 'classic':
     print('Type: Classic')
-elseif types == 'grilled':
+elif choice(types) == 'grilled':
     print('Type: Grilled')
-elseif types == 'double':
+elif choice(types) == 'double':
     print('Type: Double Decker')
-elseif types == 'bagel':
+elif choice(types) == 'bagel':
     print('Type: Bagel')
 
 print('I hope you enjoy your sandwich!')
