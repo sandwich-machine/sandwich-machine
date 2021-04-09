ba0f3b7b037441178a6638ce064bc2e706956b69
Soup <77903666+SoupDevHub@users.noreply.github.com>
2021-04-09 15:11:02 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Sandwich Machene')
+from random import *
+print('Random Sandwiches')
+
+
+
+types = ['classic', 'grilled', 'double', 'bagel']
+
+if types == 'classic':
+    print('Type: Classic')
+elif types == 'grilled':
+    print('Type: Grilled')
+elif types == 'double':
+    print('Type: Double Decker')
+elif types == 'bagel':
+    print('Type: Bagel')
+
+print('I hope you enjoy your sandwich!')
