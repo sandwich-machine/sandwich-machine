245d76855c1a0d8e4abf1904a89b4e0abfb3b45e
Soup <77903666+SoupDevHub@users.noreply.github.com>
2021-04-09 15:53:03 -0700

    Added the instructions to main.py

--- main.py
+++ main.py
@@ -4,35 +4,23 @@ print('Random Sandwiches')
 
 
 
-types = ['classic', 'grilled', 'double', 'bagel']
+types = [ 'classic', 'grilled', 'double', 'bagel' ]
+spreads = [ 'mayo', 'butter' ]
+t1 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' ]
+t2 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' ]
 
-if choice(types) == 'classic':
-    print('Type: Classic')
-elif choice(types) == 'grilled':
-    print('Type: Grilled')
-elif choice(types) == 'double':
-    print('Type: Double Decker')
-elif choice(types) == 'bagel':
-    print('Type: Bagel')
+thing1 = choice(t1)
+thing2 = choice(t2)
+spread = choice(spreads)
+type = choice(types)
 
-print('I hope you enjoy your sandwich!')
-print("""                    _.---._
-                _.-~       ~-._
-            _.-~               ~-._
-        _.-~                       ~---._
-    _.-~                                 ~\
- .-~                                    _.;
- :-._                               _.-~ ./
- `-._~-._                   _..__.-~ _.-~
-  /  ~-._~-._              / .__..--~----._
- \_____(_;-._\.        _.-~_/       ~).. . \
-    /(_____  \`--...--~_.-~______..-+_______)
-  .(_________/`--...--~/    _/nad        /\
- /-._     \_     (___./_..-~__.....__..-~./
- `-._~-._   ~\--------~  .-~_..__.-~ _.-~
-     ~-._~-._ ~---------'  / .__..--~
-         ~-._\.        _.-~_/
-             \`--...--~_.-~
-              `--...--~
-              
-""")
+if type == 'classic':
+    print(f'Type: Classic\nStep 1: Get two slices of bread.\nStep 2: {spread.title()} the bread.\nStep 3: Put {thing1} on one of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other slice on.')
+elif type == 'grilled':
+    print(f'Type: Grilled\nStep 1: Get two slices of bread.\nStep 2: {spread.title()} the bread.\nStep 3: Put {thing1} on one of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other slice on.\nStep 6: Grill the sandwich.')
+elif type == 'double':
+    print(f'Type: Double\nStep 1: Get two slices of bread.\nStep 2: {spread.title()} the bread.\nStep 3: Put {thing1} on two of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the stacks on top of each other and put the other slice on.')
+elif type == 'bagel':
+    print(f'Type: Bagel\nStep 1: Slice a bagel.\nStep 2: {spread.title()} the bagel slices.\nStep 3: Put {thing1} on one of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other slice on.')
+
+print(f'I hope you enjoy your {thing1} and {thing2} {type} sandwich!')
