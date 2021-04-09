91ddd68d88eef5eb0d2e4eef6c950ec9ff4e33a1
electron <66094410+electron271@users.noreply.github.com>
2021-04-09 17:15:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ types = ['classic', 'grilled', 'double', 'bagel']
 
 if types == 'classic':
     print('Type: Classic')
-else if types == 'grilled':
+elseif types == 'grilled':
     print('Type: Grilled')
-else if types == 'double':
+elseif types == 'double':
     print('Type: Double Decker')
-else if types == 'bagel':
+elseif types == 'bagel':
     print('Type: Bagel')
 
 print('I hope you enjoy your sandwich!')
