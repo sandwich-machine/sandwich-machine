c63a88ece08ca22b89562aeb8dfec1216632c8e9
Soup <77903666+SoupDevHub@users.noreply.github.com>
2021-08-07 10:16:25 -0700

    bagel slices

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ t2 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' , 's
 thing1 = choice(t1)
 thing2 = choice(t2)
 spread = choice(spreads)
-type = choice(types)
+sandwichtype = choice(types)
 
 if type == 'classic':
     print(f'Type: Classic\nStep 1: Get two slices of bread.\nStep 2: Spread {spread.title()} on the bread.\nStep 3: Put {thing1} on one of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other slice on.')
@@ -19,7 +19,7 @@ elif type == 'grilled':
 elif type == 'double':
     print(f'Type: Double\nStep 1: Get three slices of bread.\nStep 2: Spread {spread.title()} on the bread.\nStep 3: Put {thing1} on two of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the stacks on top of each other and put the other slice on.')
 elif type == 'bagel':
-    print(f'Type: Bagel\nStep 1: Slice a bagel.\nStep 2: Spread {spread.title()} on the bread.\nStep 3: Put {thing1} on one of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other slice on.')
+    print(f'Type: Bagel\nStep 1: Slice a bagel.\nStep 2: Spread {spread.title()} on the bagel slices.\nStep 3: Put {thing1} on one of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other slice on.')
 elif type == 'spaghetti':
     print(f'Type: Spaghetti\nStep 1: Get two spaghettis of spaghetti.\nStep 2: Spread {spread.title()} on the bread.\nStep 3: Put {thing1} on one of the spaghettis.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other spaghetti on.')
-print(f'I hope you enjoy your {thing1} and {thing2} {type} sandwich!')
+print(f'I hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!')
