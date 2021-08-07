65df69f0fe49935539215d9c18e1465ec10b82e8
electron271 <66094410+electron271@users.noreply.github.com>
2021-08-06 20:51:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ print('Random Sandwiches')
 
 
 types = [ 'classic', 'grilled', 'double', 'bagel', 'spaghetti' ]
-spreads = [ 'mayo', 'butter', 'spaghetti' ]
-t1 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' , 'spaghetti', 'chili pepper', 'ham' ]
-t2 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' , 'spaghetti', 'chili pepper', 'ham' ]
+spreads = [ 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti']
+t1 = [ 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti']
+t2 = [ 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti', 'spaghetti']
+
 
 thing1 = choice(t1)
 thing2 = choice(t2)
