490fbc45502e607b34d78753242d59f8d6d79780
electron271 <66094410+electron271@users.noreply.github.com>
2021-08-06 20:42:10 -0500

    more ingredsiantdumd

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from random import *
 print('Random Sandwiches')
 
 
-
-types = [ 'classic', 'grilled', 'double', 'bagel' ]
-spreads = [ 'mayo', 'butter' ]
-t1 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' , 'spaghetti']
-t2 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' , 'spaghetti']
+#types = [ 'classic', 'grilled', 'double', 'bagel' ]
+types = 'spaghetti'
+#spreads = [ 'mayo', 'butter' ]
+spreads = 'spaghetti'
+#t1 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' , 'spaghetti']
+#t2 = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives' , 'spaghetti']
+t1 = 'spaghetti'
+t2 = '3'
 
 thing1 = choice(t1)
 thing2 = choice(t2)
