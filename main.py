780b139c6a9f1a8927a0788021f16c82a399681e
electron271 <ahmedanzaldua@gmail.com>
2021-08-23 07:53:29 -0500

    quality control

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ print('Sandwich Machine')
 
 from random import *
 import os
+import quality_control
 types = [ 'classic', 'grilled', 'double', 'bagel' ]
-spreads = [ 'mayo', 'butter', 'peanut butter', 'avocado', 'cream cheese' ]
-t1 = [ 'cheese', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'jelly', 'honey', 'avocado', 'cucumber', 'pepperoni', 'salami', 'steak' ]
-t2 = [ 'cheese', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'jelly', 'honey', 'avocado', 'cucumber', 'pepperoni', 'salami', 'steak' ]
+
 print('github.com/SoupDevHub/Sandwich-Machine\n')
 print('''Menu
 1. Generate a sandwich
@@ -15,10 +14,10 @@ Please enter a option between 1-2 (not higher or lower, that may break our syste
 option = input()
 
 if option == "1":
-    thing1 = choice(t1)
-    thing2 = choice(t2)
-    spread = choice(spreads)
     sandwichtype = choice(types)
+    thing1 = choice(quality_control.qcthings(sandwichtype))
+    thing2 = choice(quality_control.qcthings(sandwichtype))
+    spread = choice(quality_control.qcspreads(sandwichtype))
 
     if sandwichtype == 'classic':
         print(f'Type: Classic\nStep 1: Get two slices of bread.\nStep 2: Spread {spread.title()} on the bread.\nStep 3: Put {thing1} on one of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other slice on.')
@@ -28,6 +27,7 @@ if option == "1":
         print(f'Type: Double\nStep 1: Get three slices of bread.\nStep 2: Spread {spread.title()} on the bread.\nStep 3: Put {thing1} on two of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the stacks on top of each other and put the other slice on.')
     elif sandwichtype == 'bagel':
         print(f'Type: Bagel\nStep 1: Slice a bagel.\nStep 2: Spread {spread.title()} on the bagel slices.\nStep 3: Put {thing1} on one of the slices.\nStep 4: Put {thing2} on top of {thing1}.\nStep 5: Put the other slice on.')
+    print(f'I hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!')
 if option == "2":
     print('How many sandwiches? (Too much may lag the system)')
     sandwiches = input()
@@ -38,10 +38,10 @@ if option == "2":
         os.remove("sandwiches.txt")
     f = open('sandwiches.txt', 'x')
     while currentc < sandwiches:
-        thing1 = choice(t1)
-        thing2 = choice(t2)
-        spread = choice(spreads)
         sandwichtype = choice(types)
+        thing1 = choice(quality_control.qcthings(sandwichtype))
+        thing2 = choice(quality_control.qcthings(sandwichtype))
+        spread = choice(quality_control.qcspreads(sandwichtype))
         f = open('sandwiches.txt', 'a')
         f.write(f'I hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!\n')
         currentc+=1
---
+++ quality_control.py
@@ -0,0 +1,23 @@
+from random import *
+
+def qcthings(sandwichtype):
+    if sandwichtype == 'classic':
+        things = [ 'cheese', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami', 'steak' ]
+    elif sandwichtype == 'grilled':
+        things = [ 'cheese', 'tuna', 'salmon', 'tomato', 'bacon' 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'pepperoni', 'salami', 'steak' ]
+    elif sandwichtype == 'double':
+        things = [ 'cheese', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'cucumber', 'pepperoni', 'salami' ]
+    elif sandwichtype == 'bagel':
+        things = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami']
+    return things
+
+def qcspreads(sandwichtype):
+    if sandwichtype == 'classic':
+        spreads = [ 'mayo', 'peanut butter', 'avocado', 'jelly', 'honey' ]
+    elif sandwichtype == 'grilled':
+        spreads = [ 'mayo', 'peanut butter', 'avocado', 'jelly', 'honey' ]
+    elif sandwichtype == 'double':
+        spreads = [ 'mayo', 'peanut butter', 'jelly', 'honey' ]
+    elif sandwichtype == 'bagel':
+        spreads = [ 'mayo', 'peanut butter', 'cream cheese', 'jelly', 'honey', 'avocado' ]
+    return spreads
