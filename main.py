8c5a3f2e3680ed2a04dc604072cb378768bcb7a1
electron271 <ahmedanzaldua@gmail.com>
2021-08-23 20:45:58 -0500

    zs

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-print('Sandwich Machine')
+print('Sandwich Machine') 
 
+# Load modules and types
 from random import *
 import os
 import quality_control
 import recipe
 types = [ 'classic', 'grilled', 'double', 'bagel', 'burger' ]
-print('sandwich-machine.github.io\n')
+print('sandwich-machine.github.io\n') # Website
 print('''Menu
 1. Generate a sandwich
 2. Generate a sandwich (and output it to a file)
-Please enter a option between 1-2 (not higher or lower, that may break our system)''')
+Please enter a option between 1-2 (not higher or lower, that may break our system)''') # Load Menu
 
 option = input()
 
-if option == "1":
-    sandwichtype = choice(types)
-    thing1 = choice(quality_control.qcthings(sandwichtype))
-    thing2 = choice(quality_control.qcthings(sandwichtype))
-    spread = choice(quality_control.qcspreads(sandwichtype))
+if option == "1": # Generate a sandwich
+    sandwichtype = choice(types) # Get the type
+    thing1 = choice(quality_control.qcthings(sandwichtype)) # Get thing1
+    thing2 = choice(quality_control.qcthings(sandwichtype)) # Get thing2
+    spread = choice(quality_control.qcspreads(sandwichtype)) # Get spread
 
-    print(recipe.getrecipe(sandwichtype, thing1, thing2, spread))
-    print(f'I hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!')
-if option == "2":
-    print('How many sandwiches? (Too much may lag the system)')
-    sandwiches = input()
-    sandwiches = int(sandwiches)
+    print(recipe.getrecipe(sandwichtype, thing1, thing2, spread)) # Get the recipe
+    print(f'I hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!') # Output type
+if option == "2": # Generate a sandwich (and output it to a file)
+    print('How many sandwiches? (Too much may lag the system)') # Get how many sandwiches
+    sandwiches = input() # Get input
+    sandwiches = int(sandwiches) # Convert input to a integer
     currentc = 0
     
-    if os.path.exists("sandwiches.txt"):
-        os.remove("sandwiches.txt")
-    f = open('sandwiches.txt', 'x')
-    while currentc < sandwiches:
-        sandwichtype = choice(types)
-        thing1 = choice(quality_control.qcthings(sandwichtype))
-        thing2 = choice(quality_control.qcthings(sandwichtype))
-        spread = choice(quality_control.qcspreads(sandwichtype))
-        f = open('sandwiches.txt', 'a')
-        f.write(recipe.getrecipe(sandwichtype, thing1, thing2, spread))
-        f.write(f'\nI hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!\n')
-        f.write('-----------------------------------------------\n')
-        currentc+=1
-    print("Finished outputting to sandwiches.txt! See it now!")
+    if os.path.exists("sandwiches.txt"): # If sandwiches.txt exists, DELETE IT!
+        os.remove("sandwiches.txt") # rip
+    while currentc < sandwiches: # for all sandwiches, generate a sandwich and write to file
+        sandwichtype = choice(types) # sandwichtype
+        thing1 = choice(quality_control.qcthings(sandwichtype)) # get thing1
+        thing2 = choice(quality_control.qcthings(sandwichtype)) # get thing2
+        spread = choice(quality_control.qcspreads(sandwichtype)) # get spreads
+        f = open('sandwiches.txt', 'a') # open sandwiches.txt
+        f.write(recipe.getrecipe(sandwichtype, thing1, thing2, spread)) # get recipe and write it
+        f.write(f'\nI hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!\n') # output type
+        f.write('-----------------------------------------------\n') # line
+        currentc+=1 # next sandwich
+    print("Finished outputting to sandwiches.txt! See it now!") # yay!
 
