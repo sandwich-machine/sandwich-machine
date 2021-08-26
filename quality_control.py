2e20cd3d9c1c39db2c6688425faebf203ea69d9f
electron271 <66094410+electron271@users.noreply.github.com>
2021-08-26 09:38:09 -0500

    comments

--- quality_control.py
+++ quality_control.py
@@ -1,29 +1,29 @@
 def qcthings(sandwichtype):
-    if sandwichtype == 'classic':
+    if sandwichtype == 'classic': # If sandwich type is classic, give the list of ingredients below
         things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami', 'steak' ]
-    elif sandwichtype == 'grilled':
+    elif sandwichtype == 'grilled': # If sandwich type is grilled, give the list of ingredients below
         things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'pepperoni', 'salami', 'steak' ]
-    elif sandwichtype == 'double':
+    elif sandwichtype == 'double': # If sandwich type is double, give the list of ingredients below
         things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'cucumber', 'pepperoni', 'salami' ]
-    elif sandwichtype == 'bagel':
+    elif sandwichtype == 'bagel': # If sandwich type is bagel, give the list of ingredients below
         things = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami']
-    elif sandwichtype == 'burger':
+    elif sandwichtype == 'burger': # If sandwich type is burger, give the list of ingredients below
         things = [ 'cheese', 'chicken', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'cucumber', 'steak' ]
-    else:
-        things = [ f'error, {sandwichtype} not found ' ]
-    return things
+    else: # If the sandwich type is not defined here, set the ingredients to "error, sandwichtype not found"
+        things = [ f'error, {sandwichtype} not found' ]
+    return things # Return the ingredients
 
 def qcspreads(sandwichtype):
-    if sandwichtype == 'classic':
+    if sandwichtype == 'classic':  # If sandwich type is classic, give the list of spreads below
         spreads = [ 'mayo', 'peanut butter', 'avocado', 'jelly', 'honey' ]
-    elif sandwichtype == 'grilled':
+    elif sandwichtype == 'grilled':  # If sandwich type is grilled, give the list of spreads below
         spreads = [ 'mayo', 'peanut butter', 'avocado', 'jelly', 'honey' ]
-    elif sandwichtype == 'double':
+    elif sandwichtype == 'double':  # If sandwich type is double, give the list of spreads below
         spreads = [ 'mayo', 'peanut butter', 'jelly', 'honey' ]
-    elif sandwichtype == 'bagel':
+    elif sandwichtype == 'bagel':  # If sandwich type is bagel, give the list of spreads below
         spreads = [ 'mayo', 'peanut butter', 'cream cheese', 'jelly', 'honey', 'avocado' ]
-    elif sandwichtype == 'burger':
+    elif sandwichtype == 'burger':  # If sandwich type is burger, give the list of spreads below
         spreads = [ 'mayo', 'cream cheese', 'honey' ]
-    else:
+    else: # If the sandwich type is not defined here, set the spread to "error, sandwichtype not found"
         things = [ f'error, {sandwichtype} not found ' ]
-    return spreads
+    return spreads # Return the spreads
