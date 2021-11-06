5ddd5179afe9499abe0923378af6368fbfd0c25b
electron271 <66094410+electron271@users.noreply.github.com>
2021-11-06 11:42:57 -0500

    changes

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-print('Sandwich Machine') 
+print('Sandwich Machine by SoupDevHub and electron271') 
 
 # Load modules and types
 from random import *
 import os
 import quality_control
 import recipe
+import sys
+import argparse
+
+# Technical stuff
+parser = argparse.ArgumentParser()
+parser.add_argument('-g', '--ghactions', action='store_true')
+args = parser.parse_args()
+# Technical stuff end
+
 types = [ 'classic', 'grilled', 'double', 'bagel', 'burger' ]
 print('sandwich-machine.github.io\n') # Website
 print('''Menu
@@ -12,7 +21,10 @@ print('''Menu
 2. Generate a sandwich (and output it to a file)
 Please enter a option between 1-2 (not higher or lower, that may break our system)''') # Load Menu
 
-option = input()
+if args.ghactions:
+    option = 2
+else:
+    option = input()
 
 if option == "1": # Generate a sandwich
     sandwichtype = choice(types) # Get the type
@@ -23,13 +35,16 @@ if option == "1": # Generate a sandwich
     print(recipe.getrecipe(sandwichtype, thing1, thing2, spread)) # Get the recipe
     print(f'I hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!') # Output type
 if option == "2": # Generate a sandwich (and output it to a file)
-    print('How many sandwiches? (Too much may lag the system)') # Get how many sandwiches
-    sandwiches = input() # Get input
+    if args.ghactions:
+        sandwiches = 5
+    else:
+        print('How many sandwiches? (Too much may lag the system)') # Get how many sandwiches
+        sandwiches = input() # Get input 
     sandwiches = int(sandwiches) # Convert input to a integer
     currentc = 0
     
-    if os.path.exists("sandwiches.txt"): # If sandwiches.txt exists, DELETE IT!
-        os.remove("sandwiches.txt") # rip
+    if os.path.exists("sandwiches.txt"): # If sandwiches.txt exists, delete it!
+        os.remove("sandwiches.txt")
     while currentc < sandwiches: # for all sandwiches, generate a sandwich and write to file
         sandwichtype = choice(types) # sandwichtype
         thing1 = choice(quality_control.qcthings(sandwichtype)) # get thing1
@@ -40,5 +55,5 @@ if option == "2": # Generate a sandwich (and output it to a file)
         f.write(f'\nI hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!\n') # output type
         f.write('-----------------------------------------------\n') # line
         currentc+=1 # next sandwich
-    print("Finished outputting to sandwiches.txt! See it now!") # yay!
+    print("Finished outputting to sandwiches.txt! See it now!") # confirmation
 
