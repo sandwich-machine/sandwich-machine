4e4ac402465117579e610f17ce4984214386e326
Soup <77903666+SoupDevHub@users.noreply.github.com>
2022-04-22 17:35:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,28 @@ print('sandwich-machine.github.io\n') # Website
 print('''Menu
 1. Generate a sandwich
 2. Generate a sandwich (and output it to a file)
-3. Pure Madness
-Please enter a option between 1-3''') # Load Menu
-option = input()
+Please enter a option between 1-2''') # Load Menu
+option1 = input()
+print('''Menu
+1. Quality Control
+2. P U R E  M A D N E S S
+Please enter a option between 1-2''')
+option2 = input()
 
-if option == '1': # Generate a sandwich
+if option1 == '1': # Generate a sandwich
     sandwichtype = choice(types) # Get the type
-    thing1 = choice(quality_control.qcthings(sandwichtype)) # Get thing1
-    thing2 = choice(quality_control.qcthings(sandwichtype)) # Get thing2
-    spread = choice(quality_control.qcspreads(sandwichtype)) # Get spread
+    if option2 == '1':
+        thing1 = choice(quality_control.qcthings(sandwichtype)) # Get thing1
+        thing2 = choice(quality_control.qcthings(sandwichtype)) # Get thing2
+        spread = choice(quality_control.qcspreads(sandwichtype)) # Get spread
+    if option2 == '2':
+        thing1 = choice(quality_control.pmthings(sandwichtype)) # Get thing1
+        thing2 = choice(quality_control.pmthings(sandwichtype)) # Get thing2
+        spread = choice(quality_control.pmspreads(sandwichtype)) # Get spread
 
     print(recipe.getrecipe(sandwichtype, thing1, thing2, spread)) # Get the recipe
     print(f'I hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!') # Output type
-if option == '2': # Generate a sandwich (and output it to a file)
+if option1 == '2': # Generate a sandwich (and output it to a file)
     print('How many sandwiches? (Too much may lag the system)') # Get how many sandwiches
     sandwiches = input() # Get input
     sandwiches = int(sandwiches) # Convert input to a integer
@@ -34,9 +43,14 @@ if option == '2': # Generate a sandwich (and output it to a file)
         os.remove("sandwiches.txt")
     while currentc < sandwiches: # for all sandwiches, generate a sandwich and write to file
         sandwichtype = choice(types) # sandwichtype
-        thing1 = choice(quality_control.qcthings(sandwichtype)) # get thing1
-        thing2 = choice(quality_control.qcthings(sandwichtype)) # get thing2
-        spread = choice(quality_control.qcspreads(sandwichtype)) # get spreads
+        if option2 == '1':
+            thing1 = choice(quality_control.qcthings(sandwichtype)) # get thing1
+            thing2 = choice(quality_control.qcthings(sandwichtype)) # get thing2
+            spread = choice(quality_control.qcspreads(sandwichtype)) # get spreads
+        if option2 == '2':
+            thing1 = choice(quality_control.pmthings(sandwichtype)) # Get thing1
+            thing2 = choice(quality_control.pmthings(sandwichtype)) # Get thing2
+            spread = choice(quality_control.pmspreads(sandwichtype)) # Get spread
         f = open('sandwiches.txt', 'a') # open sandwiches.txt
         f.write(recipe.getrecipe(sandwichtype, thing1, thing2, spread)) # get recipe and write it
         f.write(f'\nI hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!\n') # output type
@@ -44,11 +58,6 @@ if option == '2': # Generate a sandwich (and output it to a file)
         currentc+=1 # next sandwich
     f.write(f'sandwich-machine.github.io') # Add website to end of file
     print("Finished outputting to sandwiches.txt! See it now!") # confirmation
-if option == '3': # Generate a madness sandwich
-    sandwichtype = choice(types) # Get the type
-    thing1 = choice(quality_control.pmthings(sandwichtype)) # Get thing1
-    thing2 = choice(quality_control.pmthings(sandwichtype)) # Get thing2
-    spread = choice(quality_control.pmspreads(sandwichtype)) # Get spread
 
     print(recipe.getrecipe(sandwichtype, thing1, thing2, spread)) # Get the recipe
     print(f'I hope you enjoy your {thing1} and {thing2} {sandwichtype} sandwich with {spread}!') # Output type
