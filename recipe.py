646ab931a7421f0d386c1421056a032a3ee1364c
Soup <77903666+SoupDevHub@users.noreply.github.com>
2022-04-22 17:59:34 -0700

    TRIANGLES ARE THE SUPERIOR SANDWICH SHAPE
    its fact

--- recipe.py
+++ recipe.py
@@ -5,22 +5,25 @@ def getrecipe(sandwichtype, thing1, thing2, spread):
         Step 2: Spread {spread.title()} on the bread.
         Step 3: Put {thing1} on one of the slices.
         Step 4: Put {thing2} on top of {thing1}.
-        Step 5: Put the other slice on.'''
+        Step 5: Put the other slice on.
+        Step 6: Cut it into TRIANGLES! (superior sandwich shape)'''
     elif sandwichtype == 'grilled':
         recipe = f'''Type: Grilled
         Step 1: Get two slices of bread.
-        Step 2: Grill the bread.
-        Step 3: Spread {spread.title()} on the bread.
-        Step 4: Put {thing1} on one of the slices.
-        Step 5: Put {thing2} on top of {thing1}.
-        Step 6: Put the other slice on.'''
+        Step 2: Spread {spread.title()} on the bread.
+        Step 3: Put {thing1} on one of the slices.
+        Step 4: Put {thing2} on top of {thing1}.
+        Step 5: Put the other slice on.
+        Step 6: Grill the sandwich
+        Step 7: Cut it into TRIANGLES! (superior sandwich shape)'''
     elif sandwichtype == 'double':
         recipe = f'''Type: Double
         Step 1: Get three slices of bread.
         Step 2: Spread {spread.title()} on the bread.
         Step 3: Put {thing1} on two of the slices.
         Step 4: Put {thing2} on top of {thing1}.
-        Step 5: Put the stacks on top of each other and put the other slice on.'''
+        Step 5: Put the stacks on top of each other and put the other slice on.
+        Step 6: Cut it into TRIANGLES! (superior sandwich shape)'''
     elif sandwichtype == 'bagel':
         recipe = f'''Type: Bagel
         Step 1: Slice a bagel.
