20c7ffbdd6411c9066aba5df783d2fd19cfe82b0
Soup <77903666+SoupDevHub@users.noreply.github.com>
2022-04-22 17:11:15 -0700

    Update quality_control.py

--- quality_control.py
+++ quality_control.py
@@ -1,12 +1,12 @@
 def qcthings(sandwichtype):
     if sandwichtype == 'classic': # If sandwich type is classic, give the list of ingredients below
-        things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami', 'steak' ]
+        things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami', 'steak', 'jelly' ]
     elif sandwichtype == 'grilled': # If sandwich type is grilled, give the list of ingredients below
-        things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'pepperoni', 'salami', 'steak' ]
+        things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'avocado', 'pepperoni', 'salami', 'steak' ]
     elif sandwichtype == 'double': # If sandwich type is double, give the list of ingredients below
-        things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'cucumber', 'pepperoni', 'salami' ]
+        things = [ 'cheese', 'chicken', 'tuna', 'salmon', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami', 'steak', 'jelly' ]
     elif sandwichtype == 'bagel': # If sandwich type is bagel, give the list of ingredients below
-        things = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami']
+        things = [ 'cheese', 'tomato', 'bacon', 'lettuce', 'ham', 'avocado', 'cucumber', 'pepperoni', 'salami', 'lox', 'onions', 'jelly', 'capers' ]
     elif sandwichtype == 'burger': # If sandwich type is burger, give the list of ingredients below
         things = [ 'cheese', 'chicken', 'tomato', 'bacon', 'lettuce', 'turkey', 'pickle', 'olives', 'chili pepper', 'cucumber', 'steak' ]
     else: # If the sandwich type is not defined here, set the ingredients to "error, sandwichtype not found"
@@ -15,15 +15,15 @@ def qcthings(sandwichtype):
 
 def qcspreads(sandwichtype):
     if sandwichtype == 'classic':  # If sandwich type is classic, give the list of spreads below
-        spreads = [ 'nothing', 'mayo', 'peanut butter', 'avocado', 'jelly', 'honey' ]
+        spreads = [ 'nothing', 'mayo', 'peanut butter', 'avocado', 'honey', 'cream cheese' ]
     elif sandwichtype == 'grilled':  # If sandwich type is grilled, give the list of spreads below
-        spreads = [ 'nothing', 'mayo', 'peanut butter', 'avocado', 'jelly', 'honey' ]
+        spreads = [ 'nothing', 'mayo', 'avocado', 'honey' ]
     elif sandwichtype == 'double':  # If sandwich type is double, give the list of spreads below
-        spreads = [ 'nothing', 'mayo', 'peanut butter', 'jelly', 'honey' ]
+        spreads = [ 'nothing', 'mayo', 'peanut butter', 'avocado', 'honey', 'cream cheese' ]
     elif sandwichtype == 'bagel':  # If sandwich type is bagel, give the list of spreads below
-        spreads = [ 'nothing', 'mayo', 'peanut butter', 'cream cheese', 'jelly', 'honey', 'avocado' ]
+        spreads = [ 'nothing', 'peanut butter', 'cream cheese', 'avocado' ]
     elif sandwichtype == 'burger':  # If sandwich type is burger, give the list of spreads below
-        spreads = [ 'nothing', 'mayo', 'cream cheese', 'honey' ]
+        spreads = [ 'nothing', 'mayo', 'butter' ]
     else: # If the sandwich type is not defined here, set the spread to "error, sandwichtype not found"
         things = [ f'error, {sandwichtype} not found ' ]
     return spreads # Return the spreads
